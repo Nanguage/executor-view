242ca06dbee7b8799ea5d8d4edac180c3199a469
Nanguage <Nanguage@yahoo.com>
2022-10-06 17:48:16 +0800

    split fetch to function; add valid_job_type fetch

--- src/content/LaunchPanel.tsx
+++ src/content/LaunchPanel.tsx
@@ -10,23 +10,18 @@ import TaskCard from '../components/TaskCard'
 import { ITask } from '../types'
 
 
-interface IProps {}
-
-
-export default function LaunchPanel(props: IProps) {
+const TasksFetch = (
+      props: {
+        nRefresh: number,
+        setTasks: (tasks: ITask[]) => void
+      }
+    ) => {
+  const { setTasks, nRefresh } = props
   const serverAddr = useStore((state) => state.serverAddr)
-  const [tasks, setTasks] = React.useState<Array<ITask>>([])
   const [alertOpen, setAlertOpen] = React.useState<boolean>(false)
   const [errorText, setErrorText] = React.useState<string>("")
   const alertHidenDuration = 21000
   const fetchInterval = 20000
-
-  const handleAlertClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
-    if (reason === 'clickaway') {
-      return;
-    }
-    setAlertOpen(false);
-  };
 
   React.useEffect(() => {
     fetchTasks(serverAddr)
@@ -35,7 +30,7 @@ export default function LaunchPanel(props: IProps) {
     return () => {
       clearInterval(myInterval)
     }
-  }, [serverAddr])
+  }, [serverAddr, nRefresh])
 
   const fetchTasks = (serverAddr: string) => {
     const addr = serverAddr + "/tasks"
@@ -49,11 +44,90 @@ export default function LaunchPanel(props: IProps) {
     })
   }
 
+  const handleAlertClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setAlertOpen(false);
+  };
+
+  return (
+    <>
+      <Snackbar open={alertOpen} autoHideDuration={alertHidenDuration} onClose={handleAlertClose}>
+        <Alert onClose={handleAlertClose} severity="error" sx={{ width: '100%' }}>
+          {errorText}
+        </Alert>
+      </Snackbar>
+    </>
+  )
+}
+
+
+const ValidJobTypesFetch = (
+      props: {
+        nRefresh: number,
+        setJobTypes: (jtypes: string[]) => void
+      }
+    ) => {
+  const { setJobTypes, nRefresh } = props
+  const serverAddr = useStore((state) => state.serverAddr)
+  const [alertOpen, setAlertOpen] = React.useState<boolean>(false)
+  const [errorText, setErrorText] = React.useState<string>("")
+  const alertHidenDuration = 21000
+  const fetchInterval = 20000
+
+  React.useEffect(() => {
+    fetchValidJobTypes(serverAddr)
+    const myInterval = setInterval(() => fetchValidJobTypes(serverAddr), fetchInterval)
+
+    return () => {
+      clearInterval(myInterval)
+    }
+  }, [serverAddr, nRefresh])
+
+  const fetchValidJobTypes = (serverAddr: string) => {
+    const addr = serverAddr + "/valid_job_types"
+    axios.get(addr).then((resp) => {
+      setJobTypes(resp.data)
+    })
+    .catch((error) => {
+      console.log(error)
+      setErrorText(error.message + `: fetch ${addr}`)
+      setAlertOpen(true)
+    })
+  }
+
+  const handleAlertClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setAlertOpen(false);
+  };
+
+  return (
+    <Snackbar open={alertOpen} autoHideDuration={alertHidenDuration} onClose={handleAlertClose}>
+      <Alert onClose={handleAlertClose} severity="error" sx={{ width: '100%' }}>
+        {errorText}
+      </Alert>
+    </Snackbar>
+  )
+
+}
+
+
+export default function LaunchPanel(props: {}) {
+  const [tasks, setTasks] = React.useState<Array<ITask>>([])
+  const [validJobTypes, setValidJobTypes] = React.useState<string[]>([])
+  const [nRefresh, setNRefresh] = React.useState<number>(0)
+
   return (
     <div>
       <div>
-        <Button onClick={(e) => fetchTasks(serverAddr)}>Refresh</Button>
+        <Button onClick={(e) => {setNRefresh(nRefresh+1)}}>Refresh</Button>
       </div>
+
+      <TasksFetch nRefresh={nRefresh} setTasks={(tasks) => {setTasks(tasks)}}/>
+      <ValidJobTypesFetch nRefresh={nRefresh} setJobTypes={(jtypes) => {setValidJobTypes(jtypes)}} />
 
       <Grid container rowSpacing={1} columnSpacing={1}>
         {
@@ -66,11 +140,6 @@ export default function LaunchPanel(props: IProps) {
         }
       </Grid>
 
-      <Snackbar open={alertOpen} autoHideDuration={alertHidenDuration} onClose={handleAlertClose}>
-        <Alert onClose={handleAlertClose} severity="error" sx={{ width: '100%' }}>
-          {errorText}
-        </Alert>
-      </Snackbar>
     </div>
   )
 }
